d85ee032abf8c63aa40ee5febe26ecb972e6fe82
Linda <lindathehuang@gmail.com>
2021-11-19 21:40:00 -0800

    Removed 3rd night hero

--- script.js
+++ script.js
@@ -159,8 +159,7 @@ $( document ).ready(function() {
         } else {
             let nightImages = [
                 "resources/heroes/night1.jpg",
-                "resources/heroes/night2.jpg",
-                "resources/heroes/night3.jpg"
+                "resources/heroes/night2.jpg"
             ]
             let hero = nightImages[Math.floor(Math.random()*nightImages.length)];
             $("#hero").attr("src", hero);
